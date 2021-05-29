63b9e7d7be6f32801657cab27e467ea1f77e76b4
daisuke.nakamura <portfolio5128@gmail.com>
2021-05-29 06:59:52 +0000

    デフォルト画像(public生成)

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -68,7 +68,7 @@
                             <!--カードヘッダーここから-->
                             <div class="card-header p-3 w-100 d-flex">
                                 
-                                <img src="{{ Storage::disk('s3')->url($timeline->user->profile_image) }}" class="rounded-circle" width="50" height="50">
+                                <img src="public_profile_image/noimage.png" class="rounded-circle" width="50" height="50">
                                 
                                 <div class="ml-2 d-flex flex-column">
                                     <p class="mb-0">{{ $timeline->user->name }}</p>
@@ -87,8 +87,7 @@
                                 
                                 <div class="text-center">
                                     
-                                    <!--<img src="{{ asset('storage/images/' .$timeline->image) }}">-->
-                                    <img src="{{ Storage::disk('s3')->url($timeline->image) }}">
+                                    <img src="public_image/sample.jpg">
                                     
                                 </div>
                                 <div class="justify-content-flex-start">
